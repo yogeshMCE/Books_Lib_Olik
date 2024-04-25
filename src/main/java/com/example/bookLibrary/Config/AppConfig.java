3ab607a7568482b8d5cd6b73619509364712a3de
yogeshMCE <yogeshkumar2k19mc140dtu@gmail.com>
2024-04-25 14:31:23 +0530

    2nd Commit

--- src/main/java/com/example/bookLibrary/Config/AppConfig.java
+++ src/main/java/com/example/bookLibrary/Config/AppConfig.java
@@ -3,7 +3,9 @@ package com.example.bookLibrary.Config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @Configuration
 public class AppConfig {
     @Bean
